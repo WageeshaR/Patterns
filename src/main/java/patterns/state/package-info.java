74bfc31790e1f6f898434eff51cfc0cd0211ed9c
wageeshashilpage <wageesha@deliveryapp.com>
2023-11-29 10:31:04 +0000

    Added package-info for State pattern

---
+++ src/main/java/patterns/state/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package represents the State pattern of software development
+ * <p>
+ * Definition: The State Pattern allows an object to alter its behavior when its internal state changes.
+ *             The object will appear to change its class.
+ * <p>
+ * @author wageeshashilpage
+ * 29-11-2023
+ */
+package patterns.state;
